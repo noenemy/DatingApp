eda0def8fad1fb822f68e085538ec189de991b3b
Soonkeun Kim <noenemy@noenemys-iMac.local>
2018-09-02 19:38:47 +0900

    Fix several bugs

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
